5dd8dce44224026bd773ff9c0f9b28ffa4ad925b
Ivan Baktsheev <dot.and.thing@gmail.com>
2026-04-05 07:05:47 +0300

    feat: macos support

--- ssh/setup-user.sh
+++ ssh/setup-user.sh
@@ -2,24 +2,55 @@
 
 set -e
 
+UNAME_ARCH="$(uname -m)"
+
+OS_NAME=""
+case "$(uname -s)" in
+    Linux*)  OS_NAME="linux" ;;
+    Darwin*) OS_NAME="macos"  ;;
+    *)       echo "Unsupported OS: $(uname -s)" >&2; return 1 ;;
+esac
+
 if [[ $EUID -ne 0 ]]; then
    echo "Error: This script must be run as root or with sudo"
    exit 1
 fi
 
-apt install sudo libpam-ssh-agent-auth -y
+if [[ $OS_NAME == "linux" ]] ; then
+    apt install sudo libpam-ssh-agent-auth -y
+elif [[ $OS_NAME == "macos" ]] ; then
+    echo "Ensure you have configured terminal to allow Full Disk Access privilege"
+fi
 
 create_or_modify_user () {
     local USER_NAME="$1"
 
     if ! id "$USER_NAME" &>/dev/null; then
+        
+        if [[ $OS_NAME == "macos" ]] ; then
+            echo "Error: You need to create an user before running this script"
+            exit 1
+        fi
+        
         useradd -m -s /bin/bash -U -G sudo "$USER_NAME"
         # adduser --shell /bin/bash --disabled-password "$USER_NAME"
     fi
 
-    if ! groups $USER_NAME | grep -q sudo; then
-        usermod -a -G sudo "$USER_NAME"
+    if [[ $OS_NAME == "linux" ]] ; then
+        SUDO_GROUP="sudo"
+    elif [[ $OS_NAME == "macos" ]] ; then
+        SUDO_GROUP="admin"
     fi
+
+    if ! groups "$USER_NAME" | grep -q "$SUDO_GROUP"; then
+        if [[ $OS_NAME == "linux" ]] ; then
+            usermod -a -G sudo "$USER_NAME"
+        elif [[ $OS_NAME == "macos" ]] ; then
+            echo "Error: You need to create an admin user before running this script"
+            exit 1
+        fi
+    fi
+    
 }
 
 configure_sshd () {
@@ -51,12 +82,18 @@ configure_sshd () {
         echo "StreamLocalBindUnlink yes" > "$CONFIG_FILE_SUDO"
     fi
 
-    if ! sshd -t; then
-        echo "SSH configuration check failed"
-        exit 1
+    # TODO: check for macos
+    if [[ $OS_NAME == "linux" ]] ; then
+        if ! sshd -t; then
+            echo "SSH configuration check failed"
+            exit 1
+        fi
     fi
 
-    if systemctl reload sshd 2>/dev/null || systemctl reload ssh 2>/dev/null; then
+    if [[ $OS_NAME == "macos" ]] ; then
+    # if sudo systemsetup -setremotelogin -f off && sudo systemsetup -setremotelogin -f on ; then
+        echo "Please restart ssh manually"
+    elif systemctl reload sshd 2>/dev/null || systemctl reload ssh 2>/dev/null; then
         echo "SSH service reloaded successfully"
     elif service sshd reload 2>/dev/null || service ssh reload 2>/dev/null; then
         echo "SSH service reloaded successfully"
@@ -94,7 +131,7 @@ configure_user_ssh_access () {
 
     mkdir -p "$DOT_SSH_DIR"
     chmod 700 "$DOT_SSH_DIR"
-    chown -R $USER_NAME:$USER_NAME "$DOT_SSH_DIR"
+    chown -R "$USER_NAME":"$USER_NAME" "$DOT_SSH_DIR"
 
     if [ -f "$AUTH_KEYS" ]; then
         if ! grep -q "$PUBLIC_KEY" "$AUTH_KEYS" 2>/dev/null; then
@@ -105,7 +142,7 @@ configure_user_ssh_access () {
     fi
 
     chmod 600 "$AUTH_KEYS"
-    chown -R $USER_NAME:$USER_NAME "$AUTH_KEYS"
+    chown -R "$USER_NAME":"$USER_NAME" "$AUTH_KEYS"
 
 }
 
@@ -129,8 +166,10 @@ fi
 
 create_or_modify_user "$USER_NAME"
 configure_sshd "$CA_PUBLIC_KEY"
-configure_sudo
-configure_user_ssh_access "$USER_NAME"  "$SUDO_PUBLIC_KEY"
+if [[ $OS_NAME == "linux" ]] ; then
+    configure_sudo
+    configure_user_ssh_access "$USER_NAME"  "$SUDO_PUBLIC_KEY"
+fi
 
 
 # if [ -t 0 ] ; then
